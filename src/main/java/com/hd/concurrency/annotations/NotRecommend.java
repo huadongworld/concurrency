65b4c624c93dc9ec65aa56a669f6c82d51b686af
huadong <hd2260332898@gmail.com>
2019-09-10 16:22:13 +0800

    增加自定义注解，增加spring-boot-starter-web依赖

---
+++ src/main/java/com/hd/concurrency/annotations/NotRecommend.java
@@ -0,0 +1,19 @@
+package com.hd.concurrency.annotations;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * 用来标记【不推荐】的类或者写法
+ *
+ * @author HuaDong
+ * @date 2019/9/10 16:12
+ */
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.CLASS)
+public @interface NotRecommend {
+
+    String value() default "";
+}
