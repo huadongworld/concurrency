e86deb47ee109a4efafa30fa30981496a65bf8d9
huadong <hd2260332898@gmail.com>
2019-09-10 15:56:54 +0800

    first commit

---
+++ src/main/java/com/hd/concurrency/ConcurrencyApplication.java
@@ -0,0 +1,17 @@
+package com.hd.concurrency;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author HuaDong
+ * @date 2019/9/10 15:47
+ */
+@SpringBootApplication
+public class ConcurrencyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConcurrencyApplication.class, args);
+    }
+
+}
