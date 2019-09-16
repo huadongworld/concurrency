94160be1382f3e07bd6eee7fe334cc16085f39a2
huadong <hd2260332898@gmail.com>
2019-09-16 16:38:51 +0800

    Synchronized关键字调试优化

--- src/main/java/com/hd/concurrency/example/sync/SynchronizedExample1.java
+++ src/main/java/com/hd/concurrency/example/sync/SynchronizedExample1.java
@@ -14,18 +14,38 @@ public class SynchronizedExample1 {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(SynchronizedExample1.class);
 
-    // 修饰一个代码块
+    /**
+     * 修饰一个代码块
+     *
+     * @param j 标识
+     */
     public void test1(int j) {
         synchronized (this) {
-            for (int i = 0; i < 10; i++) {
+            for (int i = 0; i < 5; i++) {
+                try {
+                    //每次循环睡50毫秒
+                    Thread.sleep(50);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
                 LOGGER.info("test1 {} - {}", j, i);
             }
         }
     }
 
-    // 修饰一个方法
+    /**
+     * 修饰一个方法
+     *
+     * @param j 标识
+     */
     public synchronized void test2(int j) {
         for (int i = 0; i < 10; i++) {
+            try {
+                //每次循环睡50毫秒
+                Thread.sleep(50);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
             LOGGER.info("test2 {} - {}", j, i);
         }
     }
@@ -33,12 +53,13 @@ public class SynchronizedExample1 {
     public static void main(String[] args) {
         SynchronizedExample1 example1 = new SynchronizedExample1();
         SynchronizedExample1 example2 = new SynchronizedExample1();
+        //创建线程池
         ExecutorService executorService = Executors.newCachedThreadPool();
-        executorService.execute(() -> {
-            example1.test2(1);
-        });
-        executorService.execute(() -> {
-            example2.test2(2);
-        });
+        executorService.execute(
+                () -> example1.test1(1)
+        );
+        executorService.execute(
+                () -> example2.test1(2)
+        );
     }
 }
--- src/main/java/com/hd/concurrency/example/sync/SynchronizedExample2.java
+++ src/main/java/com/hd/concurrency/example/sync/SynchronizedExample2.java
@@ -14,31 +14,50 @@ public class SynchronizedExample2 {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(SynchronizedExample1.class);
 
-    // 修饰一个类
+    /**
+     * 修饰一个类
+     *
+     * @param j 标识
+     */
     public static void test1(int j) {
         synchronized (SynchronizedExample2.class) {
-            for (int i = 0; i < 10; i++) {
+            for (int i = 0; i < 5; i++) {
+                try {
+                    //每次循环睡50毫秒
+                    Thread.sleep(50);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
                 LOGGER.info("test1 {} - {}", j, i);
             }
         }
     }
 
-    // 修饰一个静态方法
+    /**
+     * 修饰一个静态方法
+     *
+     * @param j 标识
+     */
     public static synchronized void test2(int j) {
-        for (int i = 0; i < 10; i++) {
+        for (int i = 0; i < 5; i++) {
+            try {
+                //每次循环睡50毫秒
+                Thread.sleep(50);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
             LOGGER.info("test2 {} - {}", j, i);
         }
     }
 
     public static void main(String[] args) {
-        SynchronizedExample2 example1 = new SynchronizedExample2();
-        SynchronizedExample2 example2 = new SynchronizedExample2();
+        //创建线程池
         ExecutorService executorService = Executors.newCachedThreadPool();
-        executorService.execute(() -> {
-            example1.test1(1);
-        });
-        executorService.execute(() -> {
-            example2.test1(2);
-        });
+        executorService.execute(
+                () -> test2(1)
+        );
+        executorService.execute(
+                () -> test2(2)
+        );
     }
 }
