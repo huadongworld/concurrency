4cdd987f9dc6c5517988e4b19a3dd51ee6fed0f6
huadong <hd2260332898@gmail.com>
2019-10-09 14:40:02 +0800

    同步容器

---
+++ src/main/java/com/hd/concurrency/example/concurrent/ConcurrentHashMapExample.java
@@ -0,0 +1,56 @@
+package com.hd.concurrency.example.concurrent;
+
+import com.hd.concurrency.annotations.ThreadSafe;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Map;
+import java.util.concurrent.*;
+
+/**
+ * @author HuaDong
+ * @date 2019/10/9 11:04
+ */
+@ThreadSafe
+public class ConcurrentHashMapExample {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(ConcurrentHashMapExample.class);
+
+    /**
+     * 请求总数
+     */
+    public static int clientTotal = 5000;
+
+    /**
+     * 同时并发执行的线程数
+     */
+    public static int threadTotal = 200;
+
+    private static Map<Integer, Integer> map = new ConcurrentHashMap<>();
+
+    public static void main(String[] args) throws Exception {
+        ExecutorService executorService = Executors.newCachedThreadPool();
+        final Semaphore semaphore = new Semaphore(threadTotal);
+        final CountDownLatch countDownLatch = new CountDownLatch(clientTotal);
+        for (int i = 0; i < clientTotal; i++) {
+            final int count = i;
+            executorService.execute(() -> {
+                try {
+                    semaphore.acquire();
+                    update(count);
+                    semaphore.release();
+                } catch (Exception e) {
+                    LOGGER.error("exception", e);
+                }
+                countDownLatch.countDown();
+            });
+        }
+        countDownLatch.await();
+        executorService.shutdown();
+        LOGGER.info("size:{}", map.size());
+    }
+
+    private static void update(int i) {
+        map.put(i, i);
+    }
+}
